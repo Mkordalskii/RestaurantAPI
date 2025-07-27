e453cf9a5595b402161b4630a238eab73eee6e3e
Maciej <m.kordalski0@gmail.com>
2025-07-27 15:17:15 +0200

    add new methotds to DishController and Services

--- Controllers/DishController.cs
+++ Controllers/DishController.cs
@@ -18,7 +18,25 @@ namespace RestaurantAPI.Controllers
         public ActionResult Post([FromRoute] int restaurantId, [FromBody] CreateDishDto dto)
         {
             var newDishId = _dishService.Create(restaurantId, dto);
-            return Created($"api/{restaurantId}/DishService/{newDishId}", null);
+            return Created($"api/restaurant/{restaurantId}/DishService/{newDishId}", null);
+        }
+        [HttpGet("{dishId}")]
+        public ActionResult<DishDto> Get([FromRoute] int restaurantId, [FromRoute] int dishId)
+        {
+            DishDto dish = _dishService.GetById(restaurantId, dishId);
+            return Ok(dish);
+        }
+        [HttpGet]
+        public ActionResult<List<DishDto>> GetAll([FromRoute] int restaurantId)
+        {
+            var result = _dishService.GetAll(restaurantId);
+            return Ok(result);
+        }
+        [HttpDelete]
+        public ActionResult Delete([FromRoute] int restaurantId)
+        {
+            _dishService.RemoveAll(restaurantId);
+            return NoContent();
         }
     }
 }
--- Services/IDishService.cs
+++ Services/IDishService.cs
@@ -5,5 +5,8 @@ namespace RestaurantAPI.Services
     public interface IDishService
     {
         int Create(int restaurantId, CreateDishDto dto);
+        DishDto GetById(int restaurantId, int dishId);
+        List<DishDto> GetAll(int restaurantId);
+        void RemoveAll(int restaurantId);
     }
 }

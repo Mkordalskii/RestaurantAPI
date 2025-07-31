e431cae4cad1605f37a632d23b4ad26eb77e9835
Maciej <m.kordalski0@gmail.com>
2025-07-31 17:24:16 +0200

    add userLogin

--- Program.cs
+++ Program.cs
@@ -12,13 +12,40 @@ using RestaurantAPI.Services;
 using System.Reflection;
 using System.Text;
 
+// Krok 1: Utwórz builder aplikacji
 var builder = WebApplication.CreateBuilder(args);
-// NLog: Setup NLog for Dependency injection
+
+// Konfiguracja NLog
 builder.Logging.ClearProviders();
 builder.Host.UseNLog();
 
-// Add services to the container.
-//builder.Services.AddTransient<IWeatherForecastService, WeatherForecastService>();
+// Krok 2: Rejestracja wszystkich us³ug w kontenerze DI
+// UWAGA: WSZYSTKIE builder.Services.Add... musz¹ znajdowaæ siê przed builder.Build()
+
+// Rejestracja ustawieñ uwierzytelniania z appsettings.json
+var authenticationSettings = new AuthenticationSettings();
+builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);
+builder.Services.AddSingleton(authenticationSettings);
+
+// Rejestracja serwisu uwierzytelniania JWT
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "Bearer";
+    options.DefaultScheme = "Bearer";
+    options.DefaultChallengeScheme = "Bearer";
+}).AddJwtBearer(cfg =>
+{
+    cfg.RequireHttpsMetadata = true;
+    cfg.SaveToken = true;
+    cfg.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = authenticationSettings.JwtIssuer,
+        ValidAudience = authenticationSettings.JwtIssuer,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)),
+    };
+});
+
+// Rejestracja pozosta³ych us³ug
 builder.Services.AddControllers().AddFluentValidation();
 builder.Services.AddDbContext<RestaurantDbContext>();
 builder.Services.AddScoped<RestaurantSeeder>();
@@ -31,55 +58,23 @@ builder.Services.AddScoped<IDishService, DishService>();
 builder.Services.AddScoped<IAccountService, AccountService>();
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
 builder.Services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
-builder.Configuration.GetSection("Authentication");
 
-// Configure the HTTP request pipeline.
+
+// Krok 3: Budowanie aplikacji - ten krok "zamra¿a" kolekcjê us³ug
 var app = builder.Build();
+
+// Krok 4: Uruchomienie seeder'a (konfiguracja po builder.Build())
 var scope = app.Services.CreateScope();
 var seeder = scope.ServiceProvider.GetRequiredService<RestaurantSeeder>();
 seeder.Seed();
 
-var authenticationSettings = new AuthenticationSettings();
-
-app.Configuration.GetSection("Authentication").Bind(authenticationSettings);
-
-builder.Services.AddAuthentication(options =>
-
-{
-
-    options.DefaultAuthenticateScheme = "Bearer";
-
-    options.DefaultScheme = "Bearer";
-
-    options.DefaultChallengeScheme = "Bearer";
-
-}).AddJwtBearer(cfg =>
-
-{
-
-    cfg.RequireHttpsMetadata = true;
-
-    cfg.SaveToken = true;
-
-    cfg.TokenValidationParameters = new TokenValidationParameters
-
-    {
-
-        ValidIssuer = authenticationSettings.JwtIssuer,
-
-        ValidAudience = authenticationSettings.JwtIssuer,
-
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)),
-
-    };
-
-});
-
+// Krok 5: Konfiguracja potoku ¿¹dañ HTTP (middleware)
 app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseMiddleware<RequestTimeMiddleware>();
 app.UseAuthentication();
 app.UseHttpsRedirection();
 
+// Konfiguracja Swagger
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
@@ -87,7 +82,7 @@ app.UseSwaggerUI(c =>
 });
 
 app.UseAuthorization();
-
 app.MapControllers();
 
+// Krok 6: Uruchomienie aplikacji
 app.Run();

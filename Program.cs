6f54b792c779b675aa145fa53ab1ced48aba0a26
Maciej <m.kordalski0@gmail.com>
2025-08-30 18:15:31 +0200

    add RestaurantQueryValidator

--- Program.cs
+++ Program.cs
@@ -71,6 +71,7 @@ builder.Services.AddScoped<IDishService, DishService>();
 builder.Services.AddScoped<IAccountService, AccountService>();
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
 builder.Services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
+builder.Services.AddScoped<IValidator<RestaurantQuery>, RestaurantQueryValidator>();
 
 
 // Krok 3: Budowanie aplikacji - ten krok "zamra¿a" kolekcjê us³ug
